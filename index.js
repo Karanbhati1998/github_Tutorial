33e5013b370a255e7855571f11f09d3b038a935e
Karanbhati1998 <karanbhatimahepajagir@.com>
2024-02-12 18:13:41 +0530

    added somethings

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const age=24;
-const name="karan"
+const name1="karan"
 const dob=6-6-1998
 const lastname='bhati'
 function name(){
-    return `a+b + ${name}`
+    return `a+b + ${name1}`
 }
--- scripts.js
+++ scripts.js
@@ -1,1 +1,4 @@
 console.log('script.js');
+function age18(){
+console.log('age10');
+}
